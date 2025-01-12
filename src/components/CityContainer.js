c45f63ebe4cb7ec573d96973fc3cd92b56106778
Manish Singh <singhmanish1061997@gmail.com>
2025-01-12 13:47:44 +0530

    removes unused code

--- src/components/CityContainer.js
+++ src/components/CityContainer.js
@@ -5,10 +5,7 @@ export default function CityContainer({state}) {
   return (
     <div className='border border-black w-96 h-auto max-h-40 overflow-y-auto'>
       {BRANCH_LOCATOR_CITY[state]?.map((item)=>(
-        // <div className='py-2'>
           <p className='hover:bg-teal-700 hover:text-white'>{item}</p>
-          // <p className='hover:bg-green-500 hover:text-black'>{item}</p>
-
       ))}
     </div>
   )
--- src/components/State.js
+++ src/components/State.js
@@ -1,8 +1,7 @@
-import React, {useState} from 'react';
+import React from 'react';
 import CityContainer from './CityContainer';
 
 export default function State({state, showCity, setShowCity}) {
-  const [city, setCity] = useState("City");
   
   const handleClick = ()=>{
     setShowCity()
@@ -12,10 +11,10 @@ export default function State({state, showCity, setShowCity}) {
     <div className='m-2 p-2 border-b-2 border-b-gray mt-10'>
       <p className='text-teal-700 font-bold text-lg'>{state}</p>
       <div className='flex justify-between' onClick={()=>handleClick()}>
-        <span>{city}</span> 
+        <span>City</span> 
         <span><img className='h-4' alt = "acc" src = "https://www.svgrepo.com/show/409025/angle-down.svg"/></span>
       </div>
-      {showCity && < CityContainer state = {state} city = {city}/>}
+      {showCity && < CityContainer state = {state}/>}
     </div>
   )
 }
